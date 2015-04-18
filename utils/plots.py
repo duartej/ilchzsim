424c11d2bed180b4cbf950d027d9d7f8992ae8b3
duartej <jordid@post.tau.ac.il>
2015-04-18 18:12:18 +0300

    improved plots script

--- utils/plots.py
+++ utils/plots.py
@@ -1,50 +1,84 @@
-import ROOT
-ROOT.gROOT.SetBatch()
 
-primaryKaons   = lambda x: 'motherindex != -1 && abs(catchall) == '+str(x)
-secondaryKaons = lambda x: 'motherindex == -1 && abs(catchall) == '+str(x)
-Kaons = lambda x: 'abs(catchall) == '+str(x)+' && (abs(pdgId) != 3 || abs(pdgId) != 2)'
+def main(inputfile):
+    """
+    """
+    import ROOT
+    try:
+        from PyAnUtils.plotstyles import squaredStyle,setpalette
+        lstyle = squaredStyle()
+        lstyle.cd()
+        ROOT.gROOT.ForceStyle()
+        ROOT.gStyle.SetOptStat(0)
 
-f = ROOT.TFile('hzkin.root')
-t = f.Get('mctrue')
+        setpalette()
+    except ImportError:
+        pass
 
-plots = [ ('p',''),('cos(theta)',''),('p:cos(theta)','COLZ') ]
+    ROOT.gROOT.SetBatch()
+    
+    # Hadron definition
+    primaryKaons   = lambda x: 'motherindex != -1 && abs(catchall) == '+str(x)
+    secondaryKaons = lambda x: 'motherindex == -1 && abs(catchall) == '+str(x)
+    Kaons = lambda x: 'abs(catchall) == '+str(x)+' && abs(pdgId) > 100'
+    
+    # ROOT File
+    f = ROOT.TFile(inputfile)
+    if f.IsZombie():
+        mes= "ROOT file %s not found" % inputfile
+        raise IOError(mes)
 
-for res in [ 23, 25 ]:
-    c = ROOT.TCanvas()
-    c.Divide(3,4)
-
-    for i,(var,opt) in enumerate(plots):
-        c.cd(3*i+1)
-        t.Draw(var,primaryKaons(res),opt)
+    t = f.Get('mctrue')
+    
+    # Plots
+    plots = [ ('p',''),('cos(theta)',''),('p:cos(theta)','COLZ') ]
+    
+    # 23==Z, 25 == H
+    for res in [ 23, 25 ]:
+        c = ROOT.TCanvas()
+        c.Divide(3,4)
+    
+        for i,(var,opt) in enumerate(plots):
+            c.cd(3*i+1)
+            t.Draw(var,primaryKaons(res),opt)
+            
+            c.cd(3*i+2)
+            t.Draw(var,secondaryKaons(res),opt)
         
-        c.cd(3*i+2)
-        t.Draw(var,secondaryKaons(res),opt)
+            c.cd(3*i+3)
+            t.Draw(var,Kaons(res),opt)
+        
+        c.cd(3*i+4)
+        
+        t.Draw('p>>hpri(100,0,60)',primaryKaons(res))
+        hpri = ROOT.gDirectory.Get('hpri')
+        hpri.Scale(1./hpri.Integral())
+        #hpri.Sumw2()
     
-        c.cd(3*i+3)
-        t.Draw(var,Kaons(res),opt)
+        t.Draw('p>>hsec(100,0,60)',secondaryKaons(res))
+        hsec = ROOT.gDirectory.Get('hsec')
+        hsec.Scale(1./hsec.Integral())
+        #hsec.Sumw2()
     
-    c.cd(3*i+4)
+        h = hpri.Clone('dividedh')
+        #h.Sumw2()
+        h.Divide(hsec)
     
-    t.Draw('p>>hpri(100,0,60)',primaryKaons(res))
-    hpri = ROOT.gDirectory.Get('hpri')
-    hpri.Scale(1./hpri.Integral())
-    #hpri.Sumw2()
-
-    t.Draw('p>>hsec(100,0,60)',secondaryKaons(res))
-    hsec = ROOT.gDirectory.Get('hsec')
-    hsec.Scale(1./hsec.Integral())
-    #hsec.Sumw2()
-
-    h = hpri.Clone('dividedh')
-    #h.Sumw2()
-    h.Divide(hsec)
-
-    h.Draw()#"PE")
+        h.Draw()#"PE")
+    
+    
+        c.SaveAs('kaonsat'+str(res)+'.pdf')
 
 
-    c.SaveAs('kaonsat'+str(res)+'.pdf')
-
-
-
-
+if __name__ == '__main__':
+    from optparse import OptionParser,OptionGroup
+    import os
+    
+    #Opciones de entrada
+    parser = OptionParser()
+    parser.set_defaults(inputfile='hzkin.root')    
+    parser.add_option( '-i', '--inputfile', action='store', type='string', dest='inputfile',\
+            help="input root filename [hzkin.root]")
+    
+    (opt,args) = parser.parse_args()
+    
+    main(os.path.abspath(opt.inputfile))

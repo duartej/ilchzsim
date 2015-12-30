db2fee76dfd3b9391c291bf23f1c103d9472d143
duartej <jordid@post.tau.ac.il>
2015-12-30 14:33:19 +0100

    a lot of improvements in the plotting scripting, including latex tables and extra options for the user. Missing the 2d-plots

--- utils/hzplots.py
+++ utils/hzplots.py
@@ -410,8 +410,7 @@ class eff_final_state_hadrons(object):
 #        print float(eff[signal].get_total_events())/sqrt(float(sum(map(lambda (x,y): y.get_total_events(),\
 #                filter(lambda (x,y): x != signal, eff.iteritems() )))))
 
-
-def update_limits(d0_pLlist_dict,pl_attr_inst):
+def update_limits(d0_pLlist_dict,(x_ind,y_ind),pl_attr_inst):
     """
     """
     getfunc = lambda func,i: func(map(lambda x: x[i], plList))
@@ -421,16 +420,16 @@ def update_limits(d0_pLlist_dict,pl_attr_inst):
     min_y0 = 1e10
     max_y1 = 0.0
     for plList in d0_pLlist_dict.values():
-        x0 = getfunc(min,0)
+        x0 = getfunc(min,x_ind)
         if x0 < min_x0:
             min_x0 = x0*0.98
-        x1 = getfunc(max,0)
+        x1 = getfunc(max,x_ind)
         if x1 > max_x1:
             max_x1 = x1*1.02
-        y0 = getfunc(min,1)
+        y0 = getfunc(min,y_ind)
         if y0 < min_y0:
             min_y0 = y0*0.98
-        y1 = getfunc(max,1)
+        y1 = getfunc(max,y_ind)
         if y1 > max_y1:
             max_y1 = y1*1.02
 
@@ -464,8 +463,88 @@ class plot_attributes(object):
         for key,val in kwd.iteritems():
             setattr(self,key,val)
 
-def make_plot(points_dict,plot_attr,leg_position="UP"):
+def make_plot_2D(points_dict,plot_attr,aux_dict=None,leg_position="UP"):
     """
+    """
+    try:
+        from PyAnUtils.plotstyles import squaredStyle,setpalette
+        lstyle = squaredStyle()
+        lstyle.cd()
+        ROOT.gROOT.ForceStyle()
+        ROOT.gStyle.SetOptStat(0)
+
+        setpalette("darkbody")
+    except ImportError:
+        pass
+
+def get_point_graphs(points_dict,obs_indices,wp_index,working_points_list,leg_entry_format):
+    """
+    Parameters
+    ----------
+    points_dict: dict(str: [(val1,val2), ... ])
+    """
+    import ROOT
+
+    # Indices to plot
+    xI = obs_indices[0]
+    yI = obs_indices[1]
+
+    # indices for the legend points
+    wpX_I = leg_entry_format[1][0]
+    wpY_I = leg_entry_format[1][1]
+
+    graphs = {}
+    leg    = {}
+    # legend coordinates
+    y0 = 0.8; y1 = 0.94; x0 = 0.01; x1= 0.25;
+    for d0cut,val_list in points_dict.iteritems():
+        selected_points = []
+        TOLERANCE = 1e-20
+        # Extract the working points from the val_list list, assuming
+        # tuple_index as the value to check and to be equal to the 
+        # working_points_list values. Using a very small tolerance
+        # and degrading resolution if unable to find
+        n_iter = 0
+        while len(selected_points) != len(working_points_list):
+            try:
+                selected_points =  map(lambda y: 
+                        filter(lambda x: abs(x[wp_index]-y) <= TOLERANCE, val_list)[0], 
+                        working_points_list)
+            except IndexError:
+                # not found anything, so continue
+                pass
+
+            if TOLERANCE > 1.0 or n_iter > 100:
+                raise RuntimeError('Unable to find the Working Points: "{0}"'\
+                        ' Some inconsistency in the call of this function...'.\
+                        format(working_points_list))
+            TOLERANCE *= 1e1
+            n_iter += 1
+        # found the working points
+        graphs[d0cut] = []
+        # legend
+        leg[d0cut] = getleg(y0 = y0, y1 = y1, x0 = x0, x1= x1)
+        y0 -= 0.2
+        y1 -= 0.2
+        leg[d0cut].SetTextSize(0.035)
+        k = 4
+        for ntuple in selected_points:
+            graphs[d0cut].append( ROOT.TGraph() )
+            graphs[d0cut][-1].SetMarkerStyle(33)
+            graphs[d0cut][-1].SetMarkerSize(2)
+            graphs[d0cut][-1].SetMarkerColor(getcolor()[k])
+            graphs[d0cut][-1].SetPoint(0,ntuple[xI],ntuple[yI])
+            text = leg_entry_format[0] % (ntuple[wpX_I],ntuple[wpY_I])
+            leg[d0cut].AddEntry(graphs[d0cut][-1],'d0={0} mm:  {1}'.format(d0cut,text),'P')
+            k+=1
+
+    return graphs,leg
+
+
+def make_plot(points_dict,(xindex,yindex),plot_attr,g_points_dict=None,leg_position="UP"):
+    """Create a plot superimpossing the expected graphs in the points_dict
+    dictionary (one graph per each key). If aux_dict is filled, is interpreted
+    as working point dictionary and some points will be painted also
     """
     import ROOT
     
@@ -490,7 +569,7 @@ def make_plot(points_dict,plot_attr,leg_position="UP"):
         c.SetLogy()
     # una grafica para el mismo d0
     # Extract the limits
-    dummy = update_limits(points_dict,plot_attr)
+    dummy = update_limits(points_dict,(xindex,yindex),plot_attr)
     
     # Legend
     if leg_position == "DOWN":
@@ -513,13 +592,56 @@ def make_plot(points_dict,plot_attr,leg_position="UP"):
         g.SetMarkerStyle(20+k)
         leg.AddEntry(g,'d_{0}={1} mm'.format('{0}',d0cut),'PL')
         for i,val in enumerate(pL_list):
-            g.SetPoint(i,val[0],val[1])
+            g.SetPoint(i,val[xindex],val[yindex])
         g.Draw("LSAME {0}".format(plot_attr.opt))
+        if g_points_dict:
+            # Format: ( dict(str: [ROOT.TGraph, ...],..), ROOT.TLegend() )
+            # see get_points_graph to check the format
+            for _graph in g_points_dict[0][d0cut]:
+                _graph.Draw("PSAME")
+            #g_points_dict[1][d0cut].Draw()
         k+=1
     leg.Draw()
     c.SaveAs('{0}{1}'.format(plot_attr.plotname.split('.')[0],suffixplots))
+    # Legend for the points
+    if g_points_dict:
+        c.Close()
+        c.Draw()
+        for d0cut,leg in g_points_dict[1].iteritems():
+            leg.Draw()
+        c.SaveAs('{0}_legends_WP{1}'.format(plot_attr.plotname.split('.')[0],suffixplots))
 
-def main(rootfile,channels):
+def get_latex_table(obsList):
+    """.. function:: get_latex_table(obsList) -> str(latex table)
+    [ (pLcut1, eff_sig, significance, pion_rejection, purity, N_SIG, N_BKG) ]
+    """
+    columns = ( 'p_{||}^{c}\;[GeV]','\\varepsilon_{ss}', 'S/\\sqrt{B}', 'N_{bkg}/N_{s\\bar{s}}', \
+            'Kaon Purity', 'N_{s\\bar{s}}', 'N_{bkg}')
+    colformat = map(lambda x: ' ${0:'+str(x)+'}$ &' , \
+            ( '.0f','.3f','.2f','.0f','.3f', '.0f', '.0f' ))
+    ncols = len(obsList[0])
+    # Consistency
+    if len(columns) != ncols: 
+        raise RuntimeError('Unconsistency in "observable" list')
+
+    latex = '\\begin{tabular}{c '
+    for i in xrange(ncols):
+        latex += ' c '
+    latex += '}\\hline\\hline\n'
+    for i in columns:
+        latex += ' ${0}$ &'.format(i)
+    latex = latex[:-1]
+    latex += '\\\\ \\hline\n'
+    for ntuple in obsList:
+        for i,val in enumerate(ntuple):
+            latex += colformat[i].format(val)
+        latex = latex[:-1]+'\\\\\n'
+    latex += '\\hline\n'
+    latex += '\\end{tabular}\n'
+
+    return latex
+
+def main(rootfile,channels,tables,pLMax,d0cuts,wp_activated):
     """Main function gathering all the plots to be performed:
      * Plot of all the efficiencies vs. momentum cut in the
      same canvas (including the total background efficiency
@@ -553,23 +675,21 @@ def main(rootfile,channels):
     
     # just take care in the H-ressonance...
 
-    # XXX: can be entered by option
-    d0cuts = [0.1,0.3, 0.5]
-    pLcuts = xrange(0,30)
+    #d0cuts = [0.1,0.3, 0.5]
+    pLcuts = xrange(1,pLMax+1)
 
     # the eff. classes 
     eff = dict(map(lambda x: (x,eff_cut_hadron(x)), channels))
     
     message = "\r\033[1;34mhzplots INFO\033[1;m Obtaining the data"
-    # List of plots: { 'd0cut1' [ (pLcut1,valueX,valueY), ... ] }
-    effsig_vs_pion_reject = {}
-    effsig_vs_purity         = {}
-    significance_vs_pLcut    = {}
+    # { 'docut1': [ (pLcut1, eff_sig, significance, pion_rejection, purity, N_sig, N_bkg), ... ],  }
+    observables = {}
+    # Indices
+    I_PL = 0; I_EFF_SIG = 1; I_SIGN=2; I_PION_REJEC = 3; I_PURITY = 4; I_N_SIGNAL = 5; I_N_BKG=6;
+
     for d0 in d0cuts:
         d0str = '{0:.1f}'.format(d0)
-        effsig_vs_pion_reject[d0str] = []
-        effsig_vs_purity[d0str] = []
-        significance_vs_pLcut[d0str] = []
+        observables[d0str] = []
         i = 0
         # assuming the same binning for all histograms, also only TH3F in the 
         # _obj['H'] dictionary
@@ -579,8 +699,6 @@ def main(rootfile,channels):
             sys.stdout.flush()
 
             pLbin = get_bin(_obj['H'].values()[0],0,pL)
-            #print '='*80
-            #print 'd0 = {0:.1f} [mm] , pL = {1:.1f} [GeV] ======='.format(d0,pL)
             # setting the current cuts to all the efficienciesa
             _dummy = map( lambda e: e.set_total_eff(_obj['H'],pLbin=pLbin,d0bin=d0bin), eff.values())
             
@@ -589,6 +707,9 @@ def main(rootfile,channels):
             n_KK    = eff['ssbar_PID'].get_total_events()
             bkg_tot_evts = sum(map(lambda (x,y): y.get_total_events(),\
                     filter(lambda (x,y): x != signal_PID or x != signal_noPID, eff.iteritems() )))
+            # --- > probably interesting also...
+            #bkg_eff = sum(map(lambda (x,y): y.get_total_eff(),\
+            #        filter(lambda (x,y): x != signal_PID or x != signal_noPID, eff.iteritems() )))
             
             # Signal efficiency calculation: assuming 100% p-K separation with the PID 
             # effsig := eff['ssbar_PID']
@@ -596,36 +717,59 @@ def main(rootfile,channels):
             # Pion rejection: N_b/N_signal assuming no p-K separation (no PID)
             # Note: sigma_HZ*L_int*Sum_qq BR(H->qq)/Sum_qq BR(h->qq)*(Sum_qq BR(H->qq) eff_qq*f_qq)
             # the Sum_qq BR(H->qq) terms are cancelled... 
-            effsig_vs_pion_reject[d0str].append( (eff_sig,
-                    float(bkg_tot_evts)/float(n_KK)) )
-            effsig_vs_purity[d0str].append( (eff_sig,
-                    float(eff['ssbar_noPID'].get_events('KK'))/\
-                            (float(eff['ssbar_noPID'].get_events('KK'))+\
-                            float(eff['ssbar_noPID'].get_events('KP'))+\
-                            float(eff['ssbar_noPID'].get_events('PP')))
-                            ) )
-            significance_vs_pLcut[d0str].append( (pL,float(n_KK)/sqrt(float(bkg_tot_evts))) )
+            pion_rejection = float(bkg_tot_evts)/float(n_KK)
+            purity         = float(eff['ssbar_noPID'].get_events('KK'))/\
+                    (float(eff['ssbar_noPID'].get_events('KK'))+\
+                    float(eff['ssbar_noPID'].get_events('KP'))+\
+                    float(eff['ssbar_noPID'].get_events('PP')))
+            significance   = float(n_KK)/sqrt(float(bkg_tot_evts))
+
+            # store it
+            observables[d0str].append( (pL,eff_sig,significance,pion_rejection,purity,n_KK,bkg_tot_evts) )
             i+=1
     # plotting
     print
     print "\033[1;34mhzplots INFO\033[1;m Plotting..."
+    # --- Some extra points (WP)
+    if wp_activated:
+        leg_format_pion_rej = ( 'S/#sqrt{B}=%.1f @ p^{c}=%.0f GeV',(I_SIGN,I_PL) )
+        graphs_leg_pion_rej = get_point_graphs(observables,(I_EFF_SIG,I_PION_REJEC),I_PL,[10,20],leg_format_pion_rej)
+    
+        leg_format_pur = ( 'S/#sqrt{B}=%.1f @ p^{c}=%.0f GeV',(I_SIGN,I_PL) )
+        graphs_leg_pur = get_point_graphs(observables,(I_EFF_SIG,I_PURITY),I_PL,[10,20],leg_format_pur)
+        
+        leg_format_sig = ( '#varepsilon_{signal}=%.2f, #pi-rej. factor=%.1f',(I_EFF_SIG,I_PION_REJEC) )
+        graphs_leg_sig = get_point_graphs(observables,(I_PL,I_SIGN),I_EFF_SIG,[0.4,0.8],leg_format_sig)
+    else:
+        graphs_leg_pion_rej = None
+        graphs_leg_pur      = None
+        graphs_leg_sig      = None
+
     pr_attr = plot_attributes('pion_rejection',
             xtitle='#varepsilon_{S}', 
             ytitle='pion rejection (N_{BKG}/N_{s#bar{s}} with no PID)',
             x0 = 0.0, x1 = 1.0 , y0 = 0.0 )
-    make_plot(effsig_vs_pion_reject,pr_attr)
+    make_plot(observables,(I_EFF_SIG,I_PION_REJEC),pr_attr,g_points_dict=graphs_leg_pion_rej)
 
     purity_attr = plot_attributes('purity',
             xtitle='#varepsilon_{S}', 
             ytitle='purity',
             x0 = 0.0, x1 = 1.0 , y0 = 0.0, y1= 0.0 )
-    make_plot(effsig_vs_purity,purity_attr)
+    make_plot(observables,(I_EFF_SIG,I_PURITY),purity_attr,g_points_dict=graphs_leg_pur)
 
     significance_attr = plot_attributes('significance',
             xtitle='p_{||}^{c}', xunit = '[GeV]', 
             ytitle='N_{S}/#sqrt{N_{B}}',
             x0 = 0.0, y0 = 0.0 )
-    make_plot(significance_vs_pLcut,significance_attr,"DOWN")
+    make_plot(observables,(I_PL,I_SIGN),significance_attr,g_points_dict=graphs_leg_sig,leg_position="DOWN")
+
+    # Tables
+    if tables:
+        for d0,obsList in observables.iteritems():
+            print "Table for d0: {0} mm".format(d0)
+            print "----------------------"
+            print get_latex_table(obsList)
+
 
 
 if __name__ == '__main__':
@@ -634,13 +778,21 @@ if __name__ == '__main__':
 
     #Opciones de entrada
     parser = OptionParser()
-    parser.set_defaults(inputfile='processed.root')    
+    parser.set_defaults(inputfile='processed.root',pLMax=30,d0="0.1,0.3,0.5")    
     parser.add_option( '-i', '--inputfile', action='store', type='string', dest='inputfile',\
             help="input root filename [processed.root]")
     parser.add_option( '-s', '--suffix', action='store', type='string', dest='suffixout',\
             help="output suffix for the plots [.pdf]")
-    parser.add_option( '-p', '--pid', action='store_true', dest='pid',\
-            help="Assume an efficiency of 100% distinguishing between kaons and pions")
+    #parser.add_option( '-p', '--pid', action='store_true', dest='pid',\
+    #        help="Assume an efficiency of 100% distinguishing between kaons and pions")
+    parser.add_option( '-d', '--d0', action='store', type='string', dest='d0',\
+            metavar='d01[,d02,...]',help="Impact parameters cuts [0.1,0.3,0.5]")
+    parser.add_option( '-p', '--pL-cut', action='store', type='string', dest='pLMax',\
+            help="Circular momentum maximum cut [30 GeV]")
+    parser.add_option( '-t', '--tables', action='store_true', dest='tables',\
+            help="whether or not print the latex tables")
+    parser.add_option( '-w','--working-points', action='store_true', dest='wp_activate',\
+            help="whether or not plot the some working points in the plots")
     
     (opt,args) = parser.parse_args()
 
@@ -652,5 +804,8 @@ if __name__ == '__main__':
     #    channels.append('ssbar_PID')
     #else:
     #    channels.append('ssbar_noPID')
+    d0cuts = []
+    for d0cut in sorted(opt.d0.split(','),key=lambda x: float(x)):
+        d0cuts.append(float(d0cut))
 
-    main(os.path.abspath(opt.inputfile),channels)
+    main(os.path.abspath(opt.inputfile),channels,opt.tables,int(opt.pLMax),d0cuts,opt.wp_activate)

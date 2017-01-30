ef519f027f02d6b7fd176bf340a71d07aab0cc52
matthias <matthias.j.schlaffer@gmail.com>
2017-01-30 12:02:53 +0200

    add option to plottingall.sh
    allow to pass an argument to plottingall.sh which makes that the script only runs the compare_pid mode of hzplots but not the fixed_pid mode. NOTE: The fixed_pid mode has to be run before running with the compare_pid mode

--- utils/plottingall.sh
+++ utils/plottingall.sh
@@ -8,11 +8,32 @@
 # For each PID hypothesis, a folder is created.
 # 
 # To run it just :
-# ./plottingall.sh
+# ./plottingall.sh [ OPTIONS ]
+#
+# Options:
+#  -c --only-compare:  do not run fixed_pid but only collect
+#                 the pkl files and run compare_pid
 #
 #
 # jorge.duarte.campderros@cern.ch (2015-04-12)
 
+# set default values
+onlycompare=false
+
+# parse command line arguments here
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+	-c|--only-compare)
+	    onlycompare=true
+	    shift
+	    ;;
+	*)
+	    ;;
+    esac
+    shift
+done
 
 n=0
 #NCPU=`cat /proc/cpuinfo |grep processor|wc -l`
@@ -27,11 +48,14 @@ do
     #    wait;
     #    n=`ps aux|grep hzplots|wc -l`;
     #fi
-    mkdir -p ${i} ;
-    cd ${i};
-    hzplots fixed_pid -s png -z 40.0 -p 40 --pLcut-type square ${i} ../processedhz_all.root #&
+    if [[ $onlycompare = false ]]
+    then
+	mkdir -p ${i} ;
+	cd ${i};
+	hzplots fixed_pid -s png -z 40.0 -p 40 --pLcut-type square ${i} ../processedhz_all.root #&
+	cd -;
+    fi
     pklfiles=$pklfiles" $i/d0cut_dict_$i.pkl"
-    cd -;
     #n=$(($n+1))
 done;
 # Be sure everything finished
